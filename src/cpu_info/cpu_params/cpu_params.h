1d229c39b8ab3fa042ac93ef550cf96085007e49
fhawas87 <jakubwozniak775@gmail.com>
2025-08-12 20:35:32 +0200

    Gifure out how to manage creating plots for each core while tracking each 'unknown' number of CPU cores value but getting out_of_range instance. Done for today. Tommorrow I'll fight with it

--- src/cpu_info/cpu_params/cpu_params.h
+++ src/cpu_info/cpu_params/cpu_params.h
@@ -5,7 +5,11 @@
 
 
 std::vector<double> get_cpu_cores_temperatures();
-std::vector<unsigned int> get_cpu_cores_frequencies(size_t& number_of_cores);
+std::vector<unsigned int> get_cpu_cores_frequencies(const size_t& number_of_cores);
+
+//double calculate_avarage_cpu_temperature(const std::vector<double> &per_core_temperatures) {
+//  double avg = 0;
+//  fo
 
 double get_cpu_utilization();
 
--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,9 @@
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 
-#define MAX_SAMPLES_HISTORY 400
+#define MAX_SAMPLES_HISTORY 200
+
+int current_sample_index = -1;
 
 std::vector<float>                              gpu_temp_ring;
 std::vector<float>                              gpu_usage_ring;
@@ -32,12 +34,12 @@ std::vector<float>                              gpu_power_usage_ring;
 std::vector<float>                              gpu_vram_usage_ring;
 std::vector<float>                              cpu_util_ring;
 std::vector<float>                              ram_usage_ring;
-std::vector<std::vector<double>>                cpu_temps_ring;
-std::vector<std::vector<double>>                fans_speed_ring;
+std::vector<std::vector<float>>                cpu_temps_ring;
+std::vector<std::vector<float>>                fans_speed_ring;
 std::vector<std::vector<unsigned int>>          cpu_freqs_ring;
 
-int number_of_cores;
-int number_of_available_fans = 0;
+size_t number_of_cores = 0;
+size_t number_of_available_fans = 0;
 
 struct gpu_stats {
 
@@ -73,137 +75,34 @@ struct stats {
   other_stats other;
 };
 
-/*
-void gpu_info(const std::string& gpu_model_name, nvmlReturn_t initialize_result) {
-  
-  if (initialize_result == NVML_SUCCESS) {
-    printf("Result code : (%d)\n", static_cast<int>(initialize_result));
-    printf("Result info : '%s'\n", nvmlErrorString(initialize_result));
-    printf("NVML has been properly initialized\n\n"); 
-
-
-    std::vector<unsigned int>             gpu_VRAM_memory_info                       = get_gpu_VRAM_info();
-    unsigned int                          current_gpu_temp                           = get_current_gpu_temperature();
-    unsigned int                          current_gpu_core_usage                     = get_core_utilization_percentage_rate();
-    unsigned int                          current_gpu_clock_freq                     = get_gpu_clock_frequency();
-    unsigned int                          current_gpu_fan_speed                      = get_gpu_fan_speed();
-    unsigned int                          current_gpu_power_usage                    = get_gpu_power_usage();
-
-    unsigned int                          total_VRAM_installed_MiB                   = gpu_VRAM_memory_info.at(0);
-    unsigned int                          current_VRAM_used_MiB                      = gpu_VRAM_memory_info.at(1);
-    unsigned int                          current_VRAM_free_MiB                      = gpu_VRAM_memory_info.at(2);
-    float                                 current_VRAM_memory_usage                  = gpu_VRAM_memory_info.at(3);
-    
-//                                  TEST PRINT
-// <------------------------------------------------------------------------------>
-    
-    printf("------------------------------------------\n");
-    printf("%s\n", gpu_model_name.c_str());
-    printf("------------------------------------------\n");
-    printf("Temperature            : %d°C\n", current_gpu_temp);
-    printf("GPU Core usage         : %d %%\n", current_gpu_core_usage);
-    printf("VRAM memory usage      : %0.f %%\n", current_VRAM_memory_usage);
-    printf("Total VRAM installed   : %d MiB\n", total_VRAM_installed_MiB);
-    printf("Total VRAM used        : %d MiB\n", current_VRAM_used_MiB);
-    printf("Total VRAM free        : %d MiB\n", current_VRAM_free_MiB);
-    printf("GPU clock frequency    : %d MHz\n", current_gpu_clock_freq);
-    printf("GPU fan speed          : %d RPM\n", current_gpu_fan_speed);
-    printf("GPU power usage        : %d W\n", current_gpu_power_usage);
-    
-    gpu_VRAM_memory_info.clear();
-  }
-  else {
-    if (initialize_result == NVML_ERROR_DRIVER_NOT_LOADED) {
-      printf("\nNvidia driver is not runnig\n");
-      printf("ERROR code : (%d)\n", static_cast<int>(initialize_result));
-      printf("ERROR info : '%s'\n", nvmlErrorString(initialize_result));
-    }
-    if (initialize_result == NVML_ERROR_NO_PERMISSION) {
-      printf("\nNVML does not have permission to communicate to the driver\n");
-      printf("ERROR code : (%d)\n", static_cast<int>(initialize_result));
-      printf("ERROR info : '%s'\n", nvmlErrorString(initialize_result));
-    }
-    if (initialize_result == NVML_ERROR_UNKNOWN) {
-      printf("\nUnknown error !\n");
-      printf("ERROR code : (%d)\n", static_cast<int>(initialize_result));
-      printf("ERROR info : '%s'\n", nvmlErrorString(initialize_result));
-    }
-  }
-}
-
-void cpu_info(const std::string& cpu_model_name) {
-
-  std::vector<double>                     cpu_cores_temperatures                     = get_cpu_cores_temperatures();
-  size_t                                  number_of_cores                            = cpu_cores_temperatures.size();
-  std::vector<unsigned int>               cpu_cores_frequencies                      = get_cpu_cores_frequencies(number_of_cores);
-  double                                  cpu_utilization_percentage_value           = get_cpu_utilization();
-  
-  if (cpu_cores_temperatures.empty()) {
-
-    printf("Problem with reading cpu cores temperatures !\n");
-  }
-  //                                    TEST PRINT
-  // <------------------------------------------------------------------------>
-  
-  printf("-------------------------------------------");
-  printf("\n%s\n", cpu_model_name.c_str());
-  printf("-------------------------------------------");
-  printf("\nCPU utilization        : %0.f%%\n", cpu_utilization_percentage_value);
-
-  for (int core = 0; core < cpu_cores_temperatures.size(); core++) {
-
-    printf("Core %d                 : %0.f°C   %d MHz\n", core, cpu_cores_temperatures.at(core), cpu_cores_frequencies.at(core));
-  }
-  
-  cpu_cores_temperatures.clear();
-  cpu_cores_frequencies.clear();
-}
-
-void ram_info() {
-
-  unsigned int ram_usage = get_ram_memory_usage();
-  
-  printf("-------------------------------------------\n");
-  printf("RAM usage              : %d %%\n", ram_usage);
-  printf("-------------------------------------------");
-  
-}
-
-void fan_info() {
-
-  std::vector<double>                     fans_tach_info                             = get_available_fans_speed();
-
-  if (fans_tach_info.size() == 0) { printf("\nEMPTY VECTOR\n"); }
-  
-  for (int fan = 0; fan < fans_tach_info.size(); fan++) {
-    if (fans_tach_info.at(fan) == 0) { continue; }
-    printf("\nFan %d                  : %0.f RPM", fan, fans_tach_info.at(fan));
-  }
-  printf("\n-------------------------------------------\n");
-  
-}
-*/
-
-
 static inline void manage_ring_data_dec(std::vector<float> &vec, float val) { // HERE I THINK I WILL HAVE TO CLEAR VECTOR IF SIZE == MAX_SAMPLES_HISTORY
-  if (vec.size() == MAX_SAMPLES_HISTORY) { 
+  if (vec.size() >= MAX_SAMPLES_HISTORY) { 
     vec.erase(vec.begin());
   }
   vec.emplace_back(val);
 }
 
 static inline void manage_ring_data_vec_int(std::vector<std::vector<unsigned int>> &vec, const std::vector<unsigned int> &val) {
-  if (vec.size() == MAX_SAMPLES_HISTORY) {
+  if (vec.size() >= MAX_SAMPLES_HISTORY) {
     vec.erase(vec.begin());
   }
   vec.emplace_back(val);
 }
 
-static inline void manage_ring_data_vec_double(std::vector<std::vector<double>> &vec, const std::vector<double> &val) {
-  if (vec.size() == MAX_SAMPLES_HISTORY) {
-    vec.erase(vec.begin());
+static inline void manage_ring_data_vec_double(std::vector<std::vector<float>> &vec, const std::vector<double> &val) {
+  if (vec[0].size() >= MAX_SAMPLES_HISTORY) {
+    //vec.erase(vec.begin());
+    for (int i = 0; i < number_of_cores; i++) {
+      vec[i].erase(vec[i].begin());
+      //vec[i].emplace_back((float)val.at(i));
+    }
+    current_sample_index = MAX_SAMPLES_HISTORY;
   }
-  vec.emplace_back(val);
+  for (int i = 0; i < number_of_cores; i++) {
+    vec[i].emplace_back((float)val.at(i));
+  }
+  //vec.emplace_back(val);
+  //current_sample_index++;
 }
 
 static inline stats get_samples() {
@@ -218,13 +117,19 @@ static inline stats get_samples() {
   current_stats.gpu.vram_usage      = current_stats.gpu.vram_info.at(3);                manage_ring_data_dec(gpu_vram_usage_ring, (float)current_stats.gpu.vram_usage);
 
   current_stats.cpu.model           = get_cpu_model_name();                         
-  current_stats.cpu.temps           = get_cpu_cores_temperatures();                     manage_ring_data_vec_double(cpu_temps_ring, current_stats.cpu.temps);
-  size_t number_of_cores            = current_stats.cpu.temps.size();
-  current_stats.cpu.freqs           = get_cpu_cores_frequencies(number_of_cores);       manage_ring_data_vec_int(cpu_freqs_ring, current_stats.cpu.freqs);
+  current_stats.cpu.temps           = get_cpu_cores_temperatures();                     //manage_ring_data_vec_double(cpu_temps_ring, current_stats.cpu.temps);
+  number_of_cores                   = current_stats.cpu.temps.size();
+  for (int i = 0; i < number_of_cores; i++) {
+    double current_core_value = current_stats.cpu.temps.at(i);
+    cpu_temps_ring.at(i).emplace_back(current_core_value);
+  }
+                                                                                        manage_ring_data_vec_double(cpu_temps_ring, current_stats.cpu.temps);
+  //current_stats.cpu.freqs           = get_cpu_cores_frequencies(number_of_cores);       manage_ring_data_vec_int(cpu_freqs_ring, current_stats.cpu.freqs);
   current_stats.cpu.util            = get_cpu_utilization();                            manage_ring_data_dec(cpu_util_ring, (float)current_stats.cpu.util);
 
   current_stats.other.ram_usage     = get_ram_memory_usage();                           manage_ring_data_dec(ram_usage_ring, (float)current_stats.other.ram_usage);
-  current_stats.other.fans_speed    = get_available_fans_speed();                       manage_ring_data_vec_double(fans_speed_ring, current_stats.other.fans_speed);
+  //current_stats.other.fans_speed    = get_available_fans_speed();                       manage_ring_data_vec_double(fans_speed_ring, current_stats.other.fans_speed);
+  number_of_available_fans          = current_stats.other.fans_speed.size();
 
   return current_stats;
 }
@@ -275,8 +180,26 @@ static inline void draw_gpu_chart(std::string &gpu_model) {
   ImGui::End();
 }
 
+//static inline generate_and_fill_vectors(std::vector<std::vector<double>>& vec, )
+
 static inline void draw_cpu_chart(std::string &cpu_model) { 
   if (ImGui::Begin(cpu_model.c_str())) {
+    
+    //std::vector<double> current_ring_values = cpu_temps_ring.back(); // BECAUSE ALWAYS LAST VECTOR INDEX CONTAINS CURRENT DATA WITH RING APPROACH
+    for (int core = 0; core < number_of_cores; core++) {
+      //std::vector<double> current_core_ring_value = cpu_temps_ring.at(i).back();
+      std::string current_core_index = "Core " + std::to_string(core);
+      if (ImPlot::BeginPlot(current_core_index.c_str())) {
+        ImPlot::SetupAxes("t[s]", "C");
+        ImPlot::SetupAxesLimits(0, MAX_SAMPLES_HISTORY, 0, 90, ImGuiCond_Always);
+        if (!cpu_temps_ring.empty()) {
+          //std::vector<float> current_core_ring_vec = cpu_temps_ring[core];
+          ImPlot::PlotLine(current_core_index.c_str(), cpu_temps_ring[core].data(), cpu_temps_ring[core].size());
+        }
+      }
+      ImPlot::EndPlot();
+    }
+
     if (ImPlot::BeginPlot("CPU Usage")) {
       ImPlot::SetupAxes("t[s]", "%");
       ImPlot::SetupAxesLimits(0, MAX_SAMPLES_HISTORY, 0, 100, ImGuiCond_Always);
@@ -285,42 +208,8 @@ static inline void draw_cpu_chart(std::string &cpu_model) {
       }
       ImPlot::EndPlot();
     }
-    /*
-    for (int i = 0; i < number_of_cores; i++) {
-      char core_index[30];
-      snprintf(core_index, sizeof(core_index), "Core %d##plot temp", i + 1);
-      if (ImPlot::BeginPlot(core_index)) {
-        ImPlot::SetupAxes("t[s]", "C");
-        ImPlot::SetupAxesLimits(0, MAX_SAMPLES_HISTORY, 0, 90, ImGuiCond_Always);
-        if (!cpu_temps_ring.empty()) {
-          ImPLot::PlotLine(core_index, cpu_temps_ring.at(i).data(), (int)cpu_temps_ring.size());
-        }
-        ImPlot::EndPlot();
-      }
-    }
-    */
-  }
   ImGui::End();
-   
-  /*
-  if (ImGui::Begin(cpu_model.c_str())) {
-    for (int i = 0; i < number_of_cores; i++) {
-      //std::vector<double> current_core_values_vec = cpu_temps_ring.back();
-      //int current_core_vec_size = cpu_temps_ring.back().size();
-      char core_index[30];
-      snprintf(core_index, sizeof(core_index), "core %d temp", i + 1);
-      if (ImPlot::BeginPlot(core_index)) {
-        ImPlot::SetupAxes("t[s]", "C");
-        ImPlot::SetupAxesLimits(0, MAX_SAMPLES_HISTORY, 0, 90, ImGuiCond_Always);
-        if (!cpu_temps_ring.empty()) {
-          ImPlot::PlotLine(core_index, current_core_values_vec.data(), 6);
-        }
-        ImPlot::EndPlot();
-      }
-    }
   }
-  */
-  //ImGui::End();
 }
 
 static inline void draw_ram_chart() {
@@ -416,8 +305,7 @@ int main() {
     double current_time = glfwGetTime();
     if (current_time - last_time >= 1.0) {
       stats current_stats = get_samples();
-      number_of_cores = current_stats.cpu.temps.size();
-      number_of_available_fans = current_stats.other.fans_speed.size();
+      current_sample_index++;
       last_time = current_time;
     }
 
@@ -450,18 +338,6 @@ int main() {
 
   glfwDestroyWindow(window);
   glfwTerminate();
-
-
-
-  /*
-  std::string gpu_model_name = get_accessible_device_name();
-  std::string cpu_model_name = get_cpu_model_name();
-  
-  gpu_info(gpu_model_name, initialize_result);
-  cpu_info(cpu_model_name);
-  ram_info();
-  fan_info();
-  */
 
   nvmlShutdown();
   sensors_cleanup();
